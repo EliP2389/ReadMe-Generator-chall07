5585be728df47af57e936d1c0eacd7e6a05ec315
Eli Pagan <mbk8689@gmail.com>
2021-11-30 10:25:03 -0500

    added some readme questions

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
+// TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown.js')
+
 const inquirer = require('inquirer');
 
-// TODO: Include packages needed for this application
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = []
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your ReadMe'
+        },
+
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a detailed description of your project'
+        },
+
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Add screenshots of project'
+        },
+        {
+            title: 'confirm',
+            name: 'confirmCollaborators',
+            message: 'Do you want to list any collaborators on this project?',
+            default: true
+        }
+    ])
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
+
+questions()
