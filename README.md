3e2f264056e29acb267624d4a7919de343d5c1a3
Eli Pagan <mbk8689@gmail.com>
2021-12-03 13:45:00 -0500

    added badges and links for each license

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
 
 
-  # d
+  # 
 
 
 
   ## Description
-  d
+  
 
 
 
   ##  Usage
-  d
+  
 
 
   
   ## Credits
-   d
+   
 
 
   
   ## License
-  MIT
+  MIT,EPL,BSD,MPL
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+           ![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)
+           ![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)
+        //    ![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)
+  https://opensource.org/licenses/MIT
+        https://opensource.org/licenses/EPL-1.0
+        https://opensource.org/licenses/BSD-3-Clause
+        https://opensource.org/licenses/MPL-2.0
+
+  
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,45 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-   if(license === "MIT"){
-       return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)}`
+   if(license == "MIT"){
+       return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+       } 
+       if(license == "EPL"){
+       return `![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)`
+       } 
+       if(license == "BSD"){
+           return `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`
+       } 
+       if(license == "MPL"){
+           return `![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`
        }
+       if(license == "MPL", "EPL", "BSD", "MPL"){
+           return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+           ![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)
+           ![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)
+           ![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`
+       }
+       return ""
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-    if(license === "MIT"){
-        return 
-    } if(license === "NPM"){
-        return
-    } if(license === "BSD"){
-
-    }if(license === "OEM"){
-        return
-    } else {
-        return ""
+    if(license == "MIT"){
+        return `https://opensource.org/licenses/MIT`
+    } if(license == "EPL"){
+        return `https://opensource.org/licenses/EPL-1.0`
+    } if(license == "BSD"){
+        return `https://opensource.org/licenses/BSD-3-Clause`
+    }if(license == "MPL"){
+        return `https://opensource.org/licenses/MPL-2.0`
+    }if(license == "MIT", "EPL", "BSD", "MPL"){
+        return `https://opensource.org/licenses/MIT
+        https://opensource.org/licenses/EPL-1.0
+        https://opensource.org/licenses/BSD-3-Clause
+        https://opensource.org/licenses/MPL-2.0`
     }
+    
 }
 
 // TODO: Create a function that returns the license section of README
@@ -28,7 +48,8 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-   renderLicenseBadge()
+var renderBadge = renderLicenseBadge(data.license)
+var renderLink = renderLicenseLink(data.license)
   return `
 
   # ${data.title}
@@ -52,6 +73,10 @@ function generateMarkdown(data) {
   
   ## License
   ${data.license}
+  ${renderBadge}
+  ${renderLink}
+
+  
   
 `;
 }
