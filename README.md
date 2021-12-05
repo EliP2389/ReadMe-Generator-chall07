01d7d13182cc1a95fa2f8889012c6f8a2ac6e314
Eli Pagan <mbk8689@gmail.com>
2021-12-05 18:03:28 -0500

    finished the finishing touches

--- README.md
+++ README.md
@@ -1,39 +1,37 @@
 
 
   
-  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
-           ![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)
-           ![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)
-           ![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)
-  https://opensource.org/licenses/MIT
-        https://opensource.org/licenses/EPL-1.0
-        https://opensource.org/licenses/BSD-3-Clause
-        https://opensource.org/licenses/MPL-2.0
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
-  # ReadMe Generator
+  #    
 
   ## Description
-  This project displays using the backend of web development to generate a project ReadMe.md file. 
+  
 
   ## Table of contents
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Credits](#credits)
-  * [Questions](#questions)
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Credits](#Credits)
+  * [Questions](#Questions)
 
   ## Installation
-  Go to my github account, to the repositories and click on the ReadMe Generator repository. Download the ssh key or https to your local device. Then in the terminal forthe project run node index.js and you will be prompted with the ReadMe Generator questions. Once you finish, a Professional Readme will generate anddisplay.
+  
 
   ##  Usage
-  EliP2389
-  <img src="./assets/images/">
+  <img src="./assets/images/readme1-img.png">
+  <img src="./assets/images/readme2-img.png">
+
+  <a href="https://watch.screencastify.com/v/f1ToW1hfh8cVxxvjRppE">ReadMe Demo</a>
 
   ## Contributing
   
 
+  ## License
+  MIT
+
   ## Questions
-  <a href="https://github.com/EliP2389">GitHub</a>
-  You can reach me at mbk8689@gmail.com for any further questions regasrding this project.
+  <a href="https://github.com/">GitHub</a>
+  You can reach me at  for any further questions regasrding this project.
 
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,45 +2,45 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
     if (license == "MIT") {
-        return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
     }
     if (license == "EPL") {
-        return `![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)`
+        return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
     }
     if (license == "BSD") {
-        return `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`
+        return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
     }
     if (license == "MPL") {
-        return `![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`
+        return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
     }
-    if (license == "MPL", "EPL", "BSD", "MPL") {
-        return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
-           ![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)
-           ![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)
-           ![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`
-    }
+    // if (license == "MPL", "EPL", "BSD", "MPL") {
+    //     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    //     [![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
+    //     [![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+    //     [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+    // }
     return ""
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-    if (license == "MIT") {
-        return `https://opensource.org/licenses/MIT`
-    } if (license == "EPL") {
-        return `https://opensource.org/licenses/EPL-1.0`
-    } if (license == "BSD") {
-        return `https://opensource.org/licenses/BSD-3-Clause`
-    } if (license == "MPL") {
-        return `https://opensource.org/licenses/MPL-2.0`
-    } if (license == "MIT", "EPL", "BSD", "MPL") {
-        return `https://opensource.org/licenses/MIT
-        https://opensource.org/licenses/EPL-1.0
-        https://opensource.org/licenses/BSD-3-Clause
-        https://opensource.org/licenses/MPL-2.0`
-    }
+// function renderLicenseLink(license) {
+//     if (license == "MIT") {
+//         return `https://opensource.org/licenses/MIT`
+//     } if (license == "EPL") {
+//         return `https://opensource.org/licenses/EPL-1.0`
+//     } if (license == "BSD") {
+//         return `https://opensource.org/licenses/BSD-3-Clause`
+//     } if (license == "MPL") {
+//         return `https://opensource.org/licenses/MPL-2.0`
+//     } if (license == "MIT", "EPL", "BSD", "MPL") {
+//         return `https://opensource.org/licenses/MIT
+//         https://opensource.org/licenses/EPL-1.0
+//         https://opensource.org/licenses/BSD-3-Clause
+//         https://opensource.org/licenses/MPL-2.0`
+//     }
 
-}
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -49,12 +49,11 @@ function renderLicenseSection(license) { }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
     var renderBadge = renderLicenseBadge(data.license)
-    var renderLink = renderLicenseLink(data.license)
+    // var renderLink = renderLicenseLink(data.license)
     return `
 
   
   ${renderBadge}
-  ${renderLink}
 
   # ${data.title}
 
@@ -62,21 +61,25 @@ function generateMarkdown(data) {
   ${data.description}
 
   ## Table of contents
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Credits](#credits)
-  * [Questions](#questions)
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Credits](#Credits)
+  * [Questions](#Questions)
 
   ## Installation
   ${data.installation}
 
   ##  Usage
-  ${data.link}
   <img src="./assets/images/readme1-img.png">
   <img src="./assets/images/readme2-img.png">
 
+  <a href="https://watch.screencastify.com/v/f1ToW1hfh8cVxxvjRppE">ReadMe Demo</a>
+
   ## Contributing
   ${data.collaborate}
+
+  ## License
+  ${data.license}
 
   ## Questions
   <a href="https://github.com/${data.link}">GitHub</a>
